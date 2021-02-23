acae32fdec7fc44da0df247eeb3ff8e95527384f
Kumari_Sita <kumari_sita@outlook.com>
2021-02-23 00:08:19 -0500

    First Commit

---
+++ helloworld_kumari/src/test/java/helloworld_kumari/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld_kumari;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() {
+		System.out.println("... Hello World ...!");
+	}
+
+}
